0c10926857c7a1a5e1023c6ce19e99dfb3bb9d99
paulngouchet <ngouchet@bu.edu>
2020-08-02 11:00:38 -0400

    Clean Code

--- crackingTheCodingInterview/simpleLinkedList.cpp
+++ crackingTheCodingInterview/simpleLinkedList.cpp
@@ -19,8 +19,7 @@ void LinkedList::append(int value){
   node->data = value ;
   node->next = NULL
 
-  if(head == NULL)
-    {
+  if(head == NULL){
       head = node ;
     }
   else {
@@ -29,23 +28,20 @@ void LinkedList::append(int value){
     current = current->next ;
   }
   current->next = node
+  }
 }
 
-}
 void LinkedList::erase(int value){
     // i will assume there exist a linked list and that the head is not null
-    if(head->data == value)
-      {
+    if(head->data == value){
         head = head->next ;
       }
     else {
-
-    Node* current = this->head ;
-
-    while(current->next != NULL){
-      if(current->next->data == value )
-        current->next = current->next->next ;
-      current = current->next ;
+        Node* current = this->head ;
+        while(current->next != NULL){
+            if(current->next->data == value )
+               current->next = current->next->next ;
+            current = current->next ;
+        }
     }
-  }
 }
