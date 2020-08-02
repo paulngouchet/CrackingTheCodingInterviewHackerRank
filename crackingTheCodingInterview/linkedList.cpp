df0e6afdf2a3d4c18b1063945463bbd416b19216
paulngouchet <ngouchet@bu.edu>
2020-08-02 02:16:12 -0400

    Clean Code

--- crackingTheCodingInterview/linkedList.cpp
+++ crackingTheCodingInterview/linkedList.cpp
@@ -13,17 +13,13 @@ A Node is defined as:
 bool has_cycle(Node* head) {
     // Complete this function
     // Do not write the main method
-
     map<Node* , int> occurenceDictionary ;
-
     Node* current = head ;          // current is the head of the list
-
+    
     if( head == NULL )             // accessing the head
       return false ;
-
     if(head->next == NULL )     // accessing head->next
       return false ;
-
 
     while( current->next != NULL)   // this loop happens only when the linked list has more than the head. a head and another node
     {
@@ -34,29 +30,22 @@ bool has_cycle(Node* head) {
 
       if(occurenceDictionary[current] > 1)    // if traversed more than once then there is a cycle
         return true ;
-
         current = current->next ;         // the first time current is head in this loop
-
     }   // it loops through the entire list and stops at the end where now current is the end of the list , node the end of the list can't be accessed in the loop but only outside the loop. This loop without the dictionary is the blueprint for going through the entire list starting from the head
 
     if( occurenceDictionary.find(current) == occurenceDictionary.end())
       occurenceDictionary[current] = 1 ;          // accessing the last element of the linked list
     else
       occurenceDictionary[current]++ ;
-
-
+    
       map<Node*, int>::iterator it;
 
-for ( it =occurenceDictionary.begin(); it != occurenceDictionary.end(); it++ )    //looping through the entire map.
-{
+for ( it =occurenceDictionary.begin(); it != occurenceDictionary.end(); it++ ) {   //looping through the entire map.
   if(it->second > 1 )
   return true;
-
 }
-
+    
 return false ;
-
 // important note, nodes in a list can have the same value.
 // a key of a map in this can directly be the object ( struct ) Node*
-
 }
